d5ffd5efcb92da5603e0188754341f50b9f50fb6
Prince K. Akyeampong <kwekuasiedu2020@gmail.com>
2023-08-16 22:15:23 +0000

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const  express = require('express');
+const router = express.Router();
+const { authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+} = require('../controllers/userController');
+const {protect} = require('../middleware/autthMiddleware');
+
+
+router.post('/', registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
+
+module.exports = router;
